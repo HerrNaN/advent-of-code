ba56478e6a5acad3045a8fbdba569643d37b9938
Karl Wikström <karl@wikstrom.io>
2022-12-10 11:11:11 +0100

    Add template day

---
+++ kt/src/main/kotlin/days/DayYYDD.kt
@@ -0,0 +1,12 @@
+package days
+
+import Day
+import cc.ekblad.konbini.Parser
+
+class DayYYDD : Day<Any>() {
+    override fun inputParser(): Parser<Any> = TODO()
+
+    override fun solve1(input: Any): Int = TODO()
+
+    override fun solve2(input: Any): Int = TODO()
+}
---
+++ kt/src/test/kotlin/days/DayYYDDTest.kt
@@ -0,0 +1,20 @@
+package days
+
+import org.junit.jupiter.api.Test
+
+internal class DayYYDDTest {
+
+    val testInput = null
+
+    @Test
+    fun inputParser() {
+    }
+
+    @Test
+    fun solve1() {
+    }
+
+    @Test
+    fun solve2() {
+    }
+}
