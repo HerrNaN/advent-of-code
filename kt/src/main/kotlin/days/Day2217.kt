9ac07761050cf4dd6e35b56ae6897396fd9ff365
Karl Wikström <karl@wikstrom.io>
2022-12-22 11:08:04 +0100

    Add parser

--- kt/src/main/kotlin/days/Day2217.kt
+++ kt/src/main/kotlin/days/Day2217.kt
@@ -1,12 +1,21 @@
 package days
 
 import Day
-import cc.ekblad.konbini.Parser
+import cc.ekblad.konbini.*
+import common.Dir2
 
-class Day2217 : Day<Any>() {
-    override fun inputParser(): Parser<Any> = TODO()
+class Day2217 : Day<List<Dir2>>() {
+    override fun inputParser(): Parser<List<Dir2>> =
+        many1(
+            parser {
+                oneOf(
+                    parser { char('<') }.map { Dir2.Left },
+                    parser { char('>') }.map { Dir2.Right }
+                )
+            }
+        )
 
-    override fun solve1(input: Any): Int = TODO()
+    override fun solve1(input: List<Dir2>): Int = TODO()
 
-    override fun solve2(input: Any): Int = TODO()
+    override fun solve2(input: List<Dir2>): Int = TODO()
 }
--- kt/src/test/kotlin/days/Day2217Test.kt
+++ kt/src/test/kotlin/days/Day2217Test.kt
@@ -1,13 +1,63 @@
 package days
 
+import common.Dir2.Left
+import common.Dir2.Right
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class Day2217Test {
 
-    val testInput = null
+    val testInput = listOf(
+        Right,
+        Right,
+        Right,
+        Left,
+        Left,
+        Right,
+        Left,
+        Right,
+        Right,
+        Left,
+        Left,
+        Left,
+        Right,
+        Right,
+        Left,
+        Right,
+        Right,
+        Right,
+        Left,
+        Left,
+        Left,
+        Right,
+        Right,
+        Right,
+        Left,
+        Left,
+        Left,
+        Right,
+        Left,
+        Left,
+        Left,
+        Right,
+        Right,
+        Left,
+        Right,
+        Right,
+        Left,
+        Left,
+        Right,
+        Right
+    )
+
+    val testRawInput = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
     @Test
     fun inputParser() {
+        assertEquals(
+            testInput,
+            Day2217().parseInput(testRawInput)
+        )
     }
 
     @Test
